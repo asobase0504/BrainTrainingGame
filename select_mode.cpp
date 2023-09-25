b6c88a1c3aab5ca025b682216b698b35ea259968
asobase0504 <92970570+asobase0504@users.noreply.github.com>
2023-09-25 16:29:21 +0900

    セレクト画面の見た目の追加

--- select_mode.cpp
+++ select_mode.cpp
@@ -47,14 +47,22 @@ HRESULT CSelectMode::Init()
 		CObject2D* object = CObject2D::Create(CTaskGroup::EPriority::LEVEL_2D_BG);
 		object->SetPos(D3DXVECTOR3(CApplication::CENTER_X, CApplication::CENTER_Y + 20.0f, 0.0f));
 		object->SetSize(D3DXVECTOR2(587.0f * 1.6f, 419.0f * 1.6f));
-		object->SetColorAlpha(0.45f);
+		object->SetColorAlpha(0.25f);
 		object->SetTexture("TITLE_LOGO");
 	}
 
+	{
+		CObject2D* object = CObject2D::Create(CTaskGroup::EPriority::LEVEL_2D_BG);
+		object->SetPos(D3DXVECTOR3(CApplication::CENTER_X, 70.0f, 0.0f));
+		D3DXVECTOR2 size(1292.0f * 0.5f, 252.0f * 0.5f);
+		object->SetSize(size);
+		object->SetTexture("TEXT_EXPO");
+	}
+
 	CModeGuidanceItem* object;
-	object = CModeGuidanceItem::Create(D3DXVECTOR3(425.0f, 300.0f, 0.0f), CMode::MODE_TYPE::MINIGAME_REMEMBER_BEFORE);
+	object = CModeGuidanceItem::Create(D3DXVECTOR3(425.0f, 320.0f, 0.0f), CMode::MODE_TYPE::MINIGAME_REMEMBER_BEFORE);
 	object->SetModeTexture("GAME_ICON_REMENBER", "TEXT_EXPO_REMEMBER");
-	object = CModeGuidanceItem::Create(D3DXVECTOR3(975.0f, 300.0f, 0.0f), CMode::MODE_TYPE::MINIGAME_SHADOW);
+	object = CModeGuidanceItem::Create(D3DXVECTOR3(975.0f, 320.0f, 0.0f), CMode::MODE_TYPE::MINIGAME_SHADOW);
 	object->SetModeTexture("GAME_ICON_SHADOW", "TEXT_EXPO_SHADOW");
 	object = CModeGuidanceItem::Create(D3DXVECTOR3(425.0f, 600.0f, 0.0f), CMode::MODE_TYPE::MINIGAME_COMEOUT);
 	object = CModeGuidanceItem::Create(D3DXVECTOR3(975.0f, 600.0f, 0.0f), CMode::MODE_TYPE::MINIGAME_LARGE_NUMBER);
